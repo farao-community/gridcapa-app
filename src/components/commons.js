0416eb7aa0018473d86e76db47636b2cccb22c77
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2021-12-15 15:23:31 +0100

    add comment explaining the formatting of displayed date time

--- src/components/commons.js
+++ src/components/commons.js
@@ -1,5 +1,6 @@
 
 export function formatTimeStamp(timestamp) {
-    let date = new Date(timestamp).toLocaleString();
-    return date.substring(0, 10) + date.substring(12, 21);
+    let date = new Date(timestamp).toLocaleString();        // This gives something like '01/01/2020 at 12:25:30' or '01/01/2020 à 12:25:30' depending on user's computer
+    return date.substring(0, 10) + date.substring(12, 21);  // To avoid any confusion with the configured application language, this concatenates the local date with the local hour,
+                                                            // Result is something like: '01/01/2020 12:25:30'
 }
