e5b29390cde636244153d860656affa343ff9bd9
thieulinseb <sebastien.thieulin@rte-france.com>
2022-06-27 11:15:54 +0200

    Optimisation for websocket on TS and business view. Avoid to close an re oppen WS every time

--- src/components/process-timestamp-view.js
+++ src/components/process-timestamp-view.js
@@ -11,11 +11,9 @@ import TableHeader from './table-header';
 import TableCore from './table-core';
 import { useIntlRef } from '../utils/messages';
 import { useSnackbar } from 'notistack';
-import {
-    connectNotificationsWsUpdateTask,
-    fetchTimestampData,
-} from '../utils/rest-api';
+import { fetchTimestampData, getWebSocketUrl } from '../utils/rest-api';
 import { gridcapaFormatDate } from './commons';
+import useWebSocket from 'react-use-websocket';
 
 function timestampEquals(t1, t2) {
     return gridcapaFormatDate(t1) === gridcapaFormatDate(t2);
@@ -56,15 +54,11 @@ const ProcessTimestampView = ({
         [timestamp]
     );
 
-    useEffect(() => {
-        const ws = connectNotificationsWsUpdateTask(
-            updateTimestampData,
-            handleTimestampMessage
-        );
-        return function () {
-            ws.close();
-        };
-    }, [updateTimestampData, handleTimestampMessage]);
+    useWebSocket(getWebSocketUrl('task'), {
+        shouldReconnect: (closeEvent) => true,
+        onMessage: handleTimestampMessage,
+        onOpen: updateTimestampData,
+    });
 
     useEffect(() => {
         updateTimestampData();
