70d040e7fdf2b75a3f1e359ea0bbc4b3dbb6ec2c
Theo Pascoli <theo.pascoli@rte-france.com>
2024-01-16 11:37:39 +0100

    fix

--- src/components/about.js
+++ src/components/about.js
@@ -6,47 +6,47 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import Typography from '@mui/material/Typography';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Button, DialogActions, DialogContent } from '@mui/material';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogTitle,
+    Typography,
+    DialogContent,
+    DialogContentText,
+} from '@mui/material';
 import { fetchVersionAndEnvironnement } from '../utils/rest-api';
-
-const styles = {
-    title: (theme) => ({
-        padding: theme.spacing(2),
-    }),
-    grid: (theme) => ({
-        padding: theme.spacing(2),
-    }),
-};
 
 const About = ({ showAbout, hideAbout }) => {
     const [appVersion, setAppVersion] = useState(0);
 
     useEffect(() => {
-        fetchVersionAndEnvironnement().then((res) => {
-            setAppVersion(res);
-        });
+        fetchVersionAndEnvironnement()
+            .then((res) => {
+                setAppVersion(res);
+            })
+            .catch((error) => {
+                console.log('Could not retrieve version and environment');
+            });
     }, []);
 
     return (
-        <Dialog
-            open={showAbout}
-            onClose={hideAbout}
-            aria-labelledby="form-dialog-title"
-            maxWidth={'md'}
-            fullWidth={true}
-        >
-            <DialogTitle id="form-dialog-title">
-                <Typography component="span" variant="h5" sx={styles.title}>
+        <Dialog open={showAbout} onClose={hideAbout} maxWidth="md" fullWidth>
+            <DialogTitle>
+                <Typography variant="h5" align="center">
                     <FormattedMessage id="about" />
                 </Typography>
             </DialogTitle>
-            <DialogContent>`{appVersion}`</DialogContent>
+            <DialogContent dividers>
+                <DialogContentText>
+                    <FormattedMessage id="versionMessage" />
+                    {appVersion}
+                </DialogContentText>
+            </DialogContent>
             <DialogActions>
-                <Button onClick={hideAbout} variant="contained" color="primary">
+                <Button onClick={hideAbout}>
                     <FormattedMessage id="close" />
                 </Button>
             </DialogActions>
@@ -55,3 +55,8 @@ const About = ({ showAbout, hideAbout }) => {
 };
 
 export default About;
+
+About.propTypes = {
+    showAbout: PropTypes.bool.isRequired,
+    hideAbout: PropTypes.func.isRequired,
+};
