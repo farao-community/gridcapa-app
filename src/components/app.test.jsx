0b6ddd72775b224d8b81a9ce00e86332190b9890
Oualid Aloui <oualid.aloui@rte-france.com>
2024-06-23 15:50:49 +0200

    Vite migration

---
+++ src/components/app.test.jsx
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { createRoot } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app';
+import { store } from '../redux/store';
+import {
+    createTheme,
+    ThemeProvider,
+    StyledEngineProvider,
+} from '@mui/material/styles';
+import { CardErrorBoundary, SnackbarProvider } from '@gridsuite/commons-ui';
+import CssBaseline from '@mui/material/CssBaseline';
+
+let container = null;
+let root = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    root = createRoot(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    container.remove();
+    container = null;
+});
+
+it('renders', async () => {
+    await act(async () =>
+        root.render(
+            <IntlProvider locale="en">
+                <BrowserRouter>
+                    <Provider store={store}>
+                        <StyledEngineProvider injectFirst>
+                            <ThemeProvider theme={createTheme({})}>
+                                <SnackbarProvider hideIconVariant={false}>
+                                    <CssBaseline />
+                                    <CardErrorBoundary>
+                                        <App />
+                                    </CardErrorBoundary>
+                                </SnackbarProvider>
+                            </ThemeProvider>
+                        </StyledEngineProvider>
+                    </Provider>
+                </BrowserRouter>
+            </IntlProvider>,
+            container
+        )
+    );
+
+    expect(container.textContent).toContain('GridCapa');
+    act(() => {
+        root.unmount();
+        root = null;
+    });
+});
---
+++ src/components/tabs/custom-tab-panel.jsx
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import PropTypes from 'prop-types';
+
+import { Box } from '@mui/material';
+
+function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </div>
+    );
+}
+
+CustomTabPanel.propTypes = {
+    value: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired,
+};
+
+export default CustomTabPanel;
