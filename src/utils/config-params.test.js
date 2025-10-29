bd857bf02ab4b1ec6a177aa0559393f31f969214
Antoine LIMOUZIN <antoine.limouzin@rte-france.com>
2025-10-29 11:41:06 +0100

    apply prettier on tests, should build now

--- src/utils/config-params.test.js
+++ src/utils/config-params.test.js
@@ -1,6 +1,6 @@
-import { APP_NAME, COMMON_APP_NAME, getAppName } from "./config-params.js";
+import { APP_NAME, COMMON_APP_NAME, getAppName } from './config-params.js';
 
 it('should load correct appName', async () => {
-    expect(getAppName("theme")).toEqual(COMMON_APP_NAME);
-    expect(getAppName("bonjour")).toEqual(APP_NAME);
+    expect(getAppName('theme')).toEqual(COMMON_APP_NAME);
+    expect(getAppName('bonjour')).toEqual(APP_NAME);
 });
--- src/utils/date-time-utils.test.js
+++ src/utils/date-time-utils.test.js
@@ -1,4 +1,11 @@
-import { areSameDates, getDateString, getInitialTimestampToSet, getTimeString, plusOneDay, toISODate } from "./date-time-utils.js";
+import {
+    areSameDates,
+    getDateString,
+    getInitialTimestampToSet,
+    getTimeString,
+    plusOneDay,
+    toISODate,
+} from './date-time-utils.js';
 
 it('should get initial timestamp to set', async () => {
     let res = new Date(getInitialTimestampToSet('2020-01-01', '11:45', 5));
@@ -8,25 +15,36 @@ it('should get initial timestamp to set', async () => {
 });
 
 it('should transform date to date and time string', async () => {
-    let timestamp = Date.UTC(2020,0,1, 11,45,0);
+    let timestamp = Date.UTC(2020, 0, 1, 11, 45, 0);
     expect(getTimeString(new Date(timestamp))).toEqual('11:45');
     expect(getDateString(new Date(timestamp))).toEqual('2020-01-01');
 });
 
 it('should add one day', async () => {
-    let timestamp = Date.UTC(2020,0,1);
+    let timestamp = Date.UTC(2020, 0, 1);
     expect(new Date(plusOneDay(timestamp)).getDate()).toEqual(2);
 });
 
 it('should transform timestamp to ISO string', async () => {
-    let timestamp = Date.UTC(2020,0,1);
+    let timestamp = Date.UTC(2020, 0, 1);
     expect(toISODate(timestamp)).toEqual('2020-01-01');
 });
 
 it('should compare dates with or without different types', async () => {
-    let timestamp = Date.UTC(2020,0,1);
-    expect(areSameDates({timestamp: timestamp},{timestamp: timestamp})).toBe(true);
-    expect(areSameDates({timestamp: '2020-01-01T00:00Z'}, {timestamp: timestamp})).toBe(true);
-    expect(areSameDates({timestamp: '2025-01-01T00:00Z'}, {timestamp: timestamp})).toBe(false);
+    let timestamp = Date.UTC(2020, 0, 1);
+    expect(
+        areSameDates({ timestamp: timestamp }, { timestamp: timestamp })
+    ).toBe(true);
+    expect(
+        areSameDates(
+            { timestamp: '2020-01-01T00:00Z' },
+            { timestamp: timestamp }
+        )
+    ).toBe(true);
+    expect(
+        areSameDates(
+            { timestamp: '2025-01-01T00:00Z' },
+            { timestamp: timestamp }
+        )
+    ).toBe(false);
 });
-
