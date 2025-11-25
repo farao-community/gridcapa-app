579a460f2cd29e4216bd1100ce0a11afc821da4e
Antoine LIMOUZIN <antoine.limouzin@rte-france.com>
2025-11-25 14:27:33 +0100

    Move unitary tests in another PR

--- src/utils/config-params.test.js
+++
@@ -1,6 +0,0 @@
-import { APP_NAME, COMMON_APP_NAME, getAppName } from './config-params.js';
-
-it('should load correct appName', () => {
-    expect(getAppName('theme')).toEqual(COMMON_APP_NAME);
-    expect(getAppName('bonjour')).toEqual(APP_NAME);
-});
