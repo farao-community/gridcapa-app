f4f816640648c22d87ca8339b20e228f474a811b
Antoine LIMOUZIN <antoine.limouzin@rte-france.com>
2025-11-27 16:56:41 +0100

    format

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -6,12 +6,23 @@
  */
 
 import { reducer } from './reducer.js';
-import { getLocalStorageComputedLanguage, } from './local-storage.js';
-import { SELECT_COMPUTED_LANGUAGE, SELECT_LANGUAGE, SELECT_THEME } from "./actions.js";
-import { DARK_THEME, LANG_SYSTEM, LIGHT_THEME, LOGOUT_ERROR,
-    RESET_AUTHENTICATION_ROUTER_ERROR, SHOW_AUTH_INFO_LOGIN, SIGNIN_CALLBACK_ERROR, UNAUTHORIZED_USER_INFO,
-    USER_VALIDATION_ERROR
-} from "@gridsuite/commons-ui";
+import { getLocalStorageComputedLanguage } from './local-storage.js';
+import {
+    SELECT_COMPUTED_LANGUAGE,
+    SELECT_LANGUAGE,
+    SELECT_THEME,
+} from './actions.js';
+import {
+    DARK_THEME,
+    LANG_SYSTEM,
+    LIGHT_THEME,
+    LOGOUT_ERROR,
+    RESET_AUTHENTICATION_ROUTER_ERROR,
+    SHOW_AUTH_INFO_LOGIN,
+    SIGNIN_CALLBACK_ERROR,
+    UNAUTHORIZED_USER_INFO,
+    USER_VALIDATION_ERROR,
+} from '@gridsuite/commons-ui';
 
 it('should return the initial state', () => {
     const initialReducer = reducer(undefined, () => {});
@@ -30,44 +41,50 @@ it('should return the initial state', () => {
 it('should handle SELECT_THEME', () => {
     const action = {
         type: SELECT_THEME,
-        theme: LIGHT_THEME
+        theme: LIGHT_THEME,
     };
-    expect(reducer({}, action)).toEqual({theme: LIGHT_THEME});
+    expect(reducer({}, action)).toEqual({ theme: LIGHT_THEME });
 });
 
 it('should handle SELECT_LANGUAGE', () => {
     const action = {
         type: SELECT_LANGUAGE,
-        language: "Bambara"
+        language: 'Bambara',
     };
-    expect(reducer({}, action)).toEqual({language: "Bambara"});
+    expect(reducer({}, action)).toEqual({ language: 'Bambara' });
 });
 
 it('should handle SIGNIN_CALLBACK_ERROR', () => {
     const action = {
         type: SIGNIN_CALLBACK_ERROR,
-        signInCallbackError: "Error while signing in"
+        signInCallbackError: 'Error while signing in',
     };
-    expect(reducer({}, action)).toEqual({ signInCallbackError: "Error while signing in"});
+    expect(reducer({}, action)).toEqual({
+        signInCallbackError: 'Error while signing in',
+    });
 });
 
 it('should handle USER', () => {
     const action = {
-        type: "USER",
-        user: "CORESO"
+        type: 'USER',
+        user: 'CORESO',
     };
-    expect(reducer({}, action)).toEqual({ user: "CORESO"});
+    expect(reducer({}, action)).toEqual({ user: 'CORESO' });
 });
 
 it('should handle auth errors', () => {
-    [UNAUTHORIZED_USER_INFO, LOGOUT_ERROR, USER_VALIDATION_ERROR].forEach(type => {
-    const action = {
-        type: type,
-        authenticationRouterError: "Error while routing"
-    };
+    [UNAUTHORIZED_USER_INFO, LOGOUT_ERROR, USER_VALIDATION_ERROR].forEach(
+        (type) => {
+            const action = {
+                type: type,
+                authenticationRouterError: 'Error while routing',
+            };
 
-    expect(reducer({}, action)).toEqual({ authenticationRouterError: "Error while routing" });
-    });
+            expect(reducer({}, action)).toEqual({
+                authenticationRouterError: 'Error while routing',
+            });
+        }
+    );
 
     const reset = {
         type: RESET_AUTHENTICATION_ROUTER_ERROR,
@@ -79,15 +96,17 @@ it('should handle auth errors', () => {
 it('should handle SHOW_AUTH_INFO_LOGIN', () => {
     const action = {
         type: SHOW_AUTH_INFO_LOGIN,
-        showAuthenticationRouterLogin: true
+        showAuthenticationRouterLogin: true,
     };
-    expect(reducer({}, action)).toEqual({ showAuthenticationRouterLogin: true});
+    expect(reducer({}, action)).toEqual({
+        showAuthenticationRouterLogin: true,
+    });
 });
 
 it('should handle SELECT_COMPUTED_LANGUAGE', () => {
     const action = {
         type: SELECT_COMPUTED_LANGUAGE,
-        computedLanguage: "Cocoliche"
+        computedLanguage: 'Cocoliche',
     };
-    expect(reducer({}, action)).toEqual({ computedLanguage: "Cocoliche"});
+    expect(reducer({}, action)).toEqual({ computedLanguage: 'Cocoliche' });
 });
